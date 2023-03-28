7ce8a658414b80598cd334633cd8543f5c492d73
siriouthay01835680 <54681916+siriouthay01835680@users.noreply.github.com>
2023-03-28 17:36:13 -0400

    fixing up connections for github

--- js/reminders-script.js
+++ js/reminders-script.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
     $(".btn").click(function(){
         // console.log("hello");
-        window.location.href = "../new-reminder.html";
+        window.location.href = "../Sonria/pages/new-reminder.html";
     });
     $(".prev").click(function(){
         // console.log("prev");
